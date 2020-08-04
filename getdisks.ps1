bb6a6d514e137c81993ef0fae8fe60dfaadedaf8
Bekim Demiroski <bekimd@microsoft.com>
2020-08-04 19:27:58 +0800

    Update getdisks.ps1

--- getdisks.ps1
+++ getdisks.ps1
@@ -1,15 +1,17 @@
 Get-Azsdisk -Status All |  select `
-      Status, 
-
+      Status, `
+      DiskType, `
+      ProvisionSizeGB, `
+      ActualSizeGB, `
       @{Name="Volume";Expression={ $m = $_.SharePath -match "\\\\.*\\(.*)"; $matches[1] }}, `
-      @{Name="VMName";Expression={ $m = $_.ManagedBy -match "/subscriptions/(.*)/resourceGroups/(.*)/providers/Microsoft.Compute/virtualMachines/(.*)"; $matches[3] }}, `
-      @{Name="VMResourceGroup";Expression={ $m = $_.ManagedBy -match "/subscriptions/(.*)/resourceGroups/(.*)/providers/Microsoft.Compute/virtualMachines/(.*)"; $matches[2] }}, `
       @{Name="VMSubscription";Expression={ $m = $_.ManagedBy -match "/subscriptions/(.*)/resourceGroups/(.*)/providers/Microsoft.Compute/virtualMachines/(.*)"; $matches[1] }}, `
+      @{Name="VMResourceGroup";Expression={ $m = $_.ManagedBy -match "/subscriptions/(.*)/resourceGroups/(.*)/providers/Microsoft.Compute/virtualMachines/(.*)"; $matches[2] }}, `
+      @{Name="VMName";Expression={ $m = $_.ManagedBy -match "/subscriptions/(.*)/resourceGroups/(.*)/providers/Microsoft.Compute/virtualMachines/(.*)"; $matches[3] }}, `
       @{Name="DiskName";Expression={ $m = $_.UserResourceId -match "/subscriptions/(.*)/resourceGroups/(.*)/providers/Microsoft.Compute/Disks/(.*)"; $matches[3] }}, `
       @{Name="DiskResourceGroup";Expression={ $m = $_.UserResourceId -match "/subscriptions/(.*)/resourceGroups/(.*)/providers/Microsoft.Compute/Disks/(.*)"; $matches[2] }}, `
       @{Name="DiskSubscription";Expression={ $m = $_.UserResourceId -match "/subscriptions/(.*)/resourceGroups/(.*)/providers/Microsoft.Compute/Disks/(.*)"; $matches[1] }}, `
-      ProvisionSizeGB, `
-      ActualSizeGB, `
       DiskSku `
-| sort Volume
+| sort Volume, VMSubscription, VMResourceGroup, VMName, DiskName, DiskType
+
+
 
